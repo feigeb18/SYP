7f72559b1624bb9a699b1e135cc584581195307e
georg <feigeb18@htl-kaindorf.at>
2022-05-25 16:07:48 +0200

    m

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello wasgsadgdsorld!");
         System.out.println("Hello too!");
     }
 }
